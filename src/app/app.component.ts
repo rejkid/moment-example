e0379df80872b88a4dfa0eb595da72a55e38ee3e
Janusz Dalecki <rejkid@optusnet.com.au>
2022-07-09 14:51:04 +1000

    Changed syntax of importing moment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import * as moment from 'moment';
+import moment from 'moment';
 import { Moment } from 'moment';
 import { interval, map } from 'rxjs';
 
